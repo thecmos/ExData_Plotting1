b391a50ff72aab7ebe5873567df10d4038fc470c
thecmos <jloresb@hotmail.com>
2018-11-23 20:57:47 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -9,7 +9,7 @@ pfeb<-subset(read.table("household_power_consumption.txt",header=TRUE,sep=";"),a
 ##Date converted from factor to date
 pfeb_d<-mutate(pfeb,date=as.Date(dmy(as.character(Date))))
 
-##plot the histogram as required and copy in pdf file
+##plot the histogram as required and copy in png file
 hist(as.numeric(as.character(pfeb_d$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.copy(png, file = "plot1.png", bg = "white",height=480,width=480)
 
